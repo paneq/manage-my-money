16cbf8a29686ad2cb76a2a49f9c22252d44d889c
Jarosław Plebański <sejtenik@gmail.com>
2008-12-08 13:59:08 +0000

    Dodano instrukcje do hash_enums
    git-svn-id: svn+ssh://s.rootnode.pl/home/rupert/svn/manage_my_money/code@86 e449e79d-dacd-4ad4-aaaf-698cbad89799

--- lib/hash_enums.rb
+++ lib/hash_enums.rb
@@ -4,6 +4,30 @@ module HashEnums
   # enum - symbol, nazwa gettera i settera
   # types - tablica symboli dostepnych typow, lub hash dostepnych typow i ich wartosci numerycznych
   # options - hash opcji ustalajacych nazwe pola w bazie danych: :attr_suffix, :attr_prefix, :attr_name
+  #
+  # Przyklad:
+  # Definiujemy klase
+  #
+  # class User < ActiveRecord::Base
+  #   extend HashEnums
+  #   define_enum :transaction_amount_limit_type, [:transaction_count,
+  #                                                :week_count,
+  #                                                :actual_month,
+  #                                                :actual_and_last_month
+  #                                               ]
+  # end
+  #
+  # Metoda define_enum dodaje do klasy User metode statyczna TRANSACTION_AMOUNT_LIMIT_TYPES
+  # ktora zwraca w wyniku hash z symbolami tworzacymi typ wyliczeniowy oraz z przypisanymi do nich wartosciami liczbowymi
+  # W powyzszym przykladzie zakladamy ze kolumna bazy danych w ktorej przechowywana jest wartosc enum ma nazwe 'transaction_amount_limit_type_int'
+  # i jest typu integer.
+  #
+  # Dodatkowo instancje klasy User otrzymuja metody 'transaction_amount_limit_type=(symbol)' oraz 'transaction_amount_limit_type'
+  # sluzace odpowiednio do zmiany i pobierania wartosci enum (operuja na symbolach z TRANSACTION_AMOUNT_LIMIT_TYPES)
+  # Proba przypisania symbolu z poza zdefiniowanego zbioru powoduje rzucenie wyjatku.
+  #
+  #
+  #
   # author - JP
   def define_enum(enum, types_array_or_hash, options = nil)
     suffix = '_int'
