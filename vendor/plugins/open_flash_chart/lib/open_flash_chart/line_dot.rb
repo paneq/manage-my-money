bfa78a377a794bad2f7bbb69f47fbe9b18471611
Robert Pankowecki <robert.pankowecki@gmail.com>
2008-12-09 10:30:21 +0000

    ADD Open flash chart project plugin v 2
    git-svn-id: svn+ssh://s.rootnode.pl/home/rupert/svn/manage_my_money/code@92 e449e79d-dacd-4ad4-aaaf-698cbad89799

---
+++ vendor/plugins/open_flash_chart/lib/open_flash_chart/line_dot.rb
@@ -0,0 +1,18 @@
+module OpenFlashChart
+
+  class LineDot < LineBase
+    def initialize args={}
+      super
+      @type = "line_dot"
+    end
+  end
+
+  class DotValue < Base
+    def initialize(value, colour, args={})
+      @value = value
+      @colour = colour
+      super
+    end
+  end
+
+end
