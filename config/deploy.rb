3a2100bf9b0a06afc0e39673e5efdeb7f3463bc7
Robert Pankowecki <robert.pankowecki@gmail.com>
2009-01-15 11:14:57 +0000

    Pierwsza konfiguracja deployowania aplikacji
    git-svn-id: svn+ssh://s.rootnode.pl/home/rupert/svn/manage_my_money/code@160 e449e79d-dacd-4ad4-aaaf-698cbad89799

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,19 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "manage_my_money"
+set :repository,  "svn://3m.dev.pankowecki.pl:3033"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/home/rupert/apps/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :subversion
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+set :user, "rupert"
+set :scm_username, "rupert"
+set :use_sudo, false
+
+role :app, "s.rootnode.pl"
+role :web, "s.rootnode.pl"
+role :db,  "s.rootnode.pl", :primary => true
