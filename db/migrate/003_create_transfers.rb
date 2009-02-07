8384cd2d24ffaf86c41342799838fa25d93f6756
Robert Pankowecki <robert.pankowecki@gmail.com>
2009-02-07 13:41:27 +0000

    Drobniutkie poprawki.
    git-svn-id: svn+ssh://s.rootnode.pl/home/rupert/svn/manage_my_money/code@253 e449e79d-dacd-4ad4-aaaf-698cbad89799

--- db/migrate/003_create_transfers.rb
+++ db/migrate/003_create_transfers.rb
@@ -1,9 +1,9 @@
 class CreateTransfers < ActiveRecord::Migration
   def self.up
     create_table :transfers do |t|
-		t.column :description, :text, :null => false
-		t.column :day, :date, :null => false
-		t.column :user_id, :integer, :null => false
+      t.column :description, :text, :null => false
+      t.column :day, :date, :null => false
+      t.column :user_id, :integer, :null => false
     end
   end
 
