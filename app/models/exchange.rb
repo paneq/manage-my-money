7ff4163f7706de7bb5c40cb0b34d9fa95df9ca38
Robert Pankowecki <robert.pankowecki@gmail.com>
2009-02-10 16:50:27 +0000

    Kursy. Model i testy do modelu.
    git-svn-id: svn+ssh://s.rootnode.pl/home/rupert/svn/manage_my_money/code@293 e449e79d-dacd-4ad4-aaaf-698cbad89799

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -14,66 +14,46 @@
 
 class Exchange < ActiveRecord::Base
   belongs_to :left_currency,
-  :class_name => "Currency",
-  :foreign_key => "currency_a"
+    :class_name => "Currency",
+    :foreign_key => "currency_a"
   
   belongs_to :right_currency,
-  :class_name => "Currency",
-  :foreign_key => "currency_b"             
+    :class_name => "Currency",
+    :foreign_key => "currency_b"
   
   belongs_to  :user
-  
-  alias_method :original_save, :save
 
-  ##############################
-  # @author: Robert Pankowecki
+  validates_presence_of :left_to_right, :right_to_left
+  validates_numericality_of :left_to_right, :right_to_left, :greater_than => 0
+  validates_presence_of :left_currency, :right_currency, :day
+
+  #alias_method :original_save, :save
+
   def currencies
     return (left_currencies.to_a + right_currencies.to_a).uniq
   end
   
-  ##############################
-  # @author: Robert Pankowecki
-  def save
+
+  def before_validation
     self.left_currency, self.right_currency, self.left_to_right, self.right_to_left = self.right_currency, self.left_currency, self.right_to_left, self.left_to_right if self.left_currency and self.right_currency and self.left_currency.id > self.right_currency.id
-    original_save
+    #original_save
   end
   
-  ##############################
-  # @author: Robert Pankowecki
+
   def validate
-    not_filled_fields { |error_message| errors.add(error_message) }
-    errors.add("Same currencies") if same_currencies?
-    errors.add("currencies order") if wrong_currencies_order?
+    errors.add_to_base(:wrong_order) if wrong_currencies_order?
+    errors.add(:right_currency, :same_as_left) if same_currencies?
   end
   
-  ##############################
-  # @author: Robert Pankowecki
+
   def same_currencies?
-    return true if self.left_currency and self.right_currency and self.left_currency.id == self.right_currency.id
+    return true if self.left_currency.is_a?(Currency) and self.right_currency.is_a?(Currency) and self.left_currency.id == self.right_currency.id
+    return false
   end  
   
-  ##############################
-  # @author: Robert Pankowecki
+
   def wrong_currencies_order?
-    return self.left_currency.id > self.right_currency.id
+    return self.left_currency.is_a?(Currency) && self.right_currency.is_a?(Currency) && self.left_currency.id > self.right_currency.id
   end
   
-  ##############################
-  # @author: Robert Pankowecki
-  # @desctiption : I block is given yields an error message for each
-  #                field that is empty, otherwise returns an array of error messages
-  def not_filled_fields(&proc)
-    tb = []
-    unless Kernel.block_given?
-      block = Proc.new { |message| tb << message }
-    else
-      block = proc
-    end
-    
-    block.call('Exchange from first to second') if left_to_right == nil or left_to_right <= 0
-    block.call('Exchange from second to first') if right_to_left == nil or right_to_left <= 0
-    
-    return if Kernel.block_given?
-    return tb
-  end
 end
--- test/unit/exchange_test.rb
+++ test/unit/exchange_test.rb
@@ -1,10 +1,55 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ExchangeTest < Test::Unit::TestCase
-#  fixtures :exchanges
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def setup
+    save_currencies
+    save_rupert
   end
+
+
+  def test_validation
+    e1 = Exchange.new(:left_to_right => 1.2, :right_to_left => 0.12, :left_currency => @euro, :right_currency => @zloty, :day => Date.today, :user => @rupert)
+    assert e1.save
+    e2 = Exchange.new(:left_to_right => 1.2, :right_to_left => 0.12, :left_currency => @zloty, :right_currency => @euro, :day => Date.today, :user => @rupert)
+    assert e2.save
+      
+    assert e1.left_currency.id < e1.right_currency.id
+    assert e2.left_currency.id < e2.right_currency.id
+    
+
+    e = Exchange.new()
+    assert !e.valid?
+    [:left_to_right, :right_to_left, :left_currency, :right_currency, :day].each do |field|
+      assert_not_nil e.errors.on(field)
+    end
+
+    e = Exchange.new(:left_to_right => 1.2, :right_to_left => 0.8)
+    assert !e.valid?
+    assert_nil e.errors.on(:left_to_right)
+    assert_nil e.errors.on(:right_to_left)
+
+    e = Exchange.new(:left_to_right => -1.2, :right_to_left => 0.0)
+    assert !e.valid?
+    assert_not_nil e.errors.on(:left_to_right)
+    assert_not_nil e.errors.on(:right_to_left)
+
+
+    e = Exchange.new(:left_currency => @zloty, :right_currency => @zloty)
+    assert !e.valid?
+    assert_nil e.errors.on(:left_currency)
+    assert_not_nil e.errors.on(:right_currency)
+
+    Exchange.send(:define_method, :before_validation) do
+    end
+
+    tbl = [@zloty, @euro]
+    tbl.sort! {|a,b| a.id <=> b.id}
+    e = Exchange.new(:left_to_right => 1.2, :right_to_left => 0.12, :left_currency => tbl.second, :right_currency => tbl.first, :day => Date.today, :user => @rupert)
+
+    assert !e.valid?
+    assert_not_nil e.errors.on(:base)
+    
+  end
+
 end
