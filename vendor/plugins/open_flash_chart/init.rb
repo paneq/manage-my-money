ff919f35444bd4bc8e1ad6124ac6cf7553432ea6
Robert Pankowecki <robert.pankowecki@gmail.com>
2009-02-25 08:48:21 +0000

    Query trace plugin.
    git-svn-id: svn+ssh://s.rootnode.pl/home/rupert/svn/manage_my_money/code@341 e449e79d-dacd-4ad4-aaaf-698cbad89799

--- vendor/plugins/open_flash_chart/init.rb
+++
@@ -1,6 +0,0 @@
-require 'open_flash_chart'
-
-ActionView::Base.send :include, OpenFlashChart::View
-OpenFlashChart::Base.send :include, OpenFlashChart::View
-ActionController::Base.send :include, OpenFlashChart::Controller
-ActionController::Base.send :include, OpenFlashChart
