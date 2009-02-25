8138e0c0b32b6d2dc721604f5949a158dc932833
Robert Pankowecki <robert.pankowecki@gmail.com>
2009-02-25 11:04:52 +0000

    Poprawki do testow LoanControllera dla sprawdzania wartosci wierzycieli.
    git-svn-id: svn+ssh://s.rootnode.pl/home/rupert/svn/manage_my_money/code@344 e449e79d-dacd-4ad4-aaaf-698cbad89799

--- test/functional/loans_controller_test.rb
+++ test/functional/loans_controller_test.rb
@@ -8,6 +8,7 @@ class LoansControllerTest < ActionController::TestCase
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
 
+    save_currencies
     save_rupert
     log_rupert
   end
@@ -24,20 +25,27 @@ class LoansControllerTest < ActionController::TestCase
 
     #my debtor
     debtor_transfer = save_simple_transfer(:outcome => rupert.asset, :income => rupert.loan.children.first) #move money to jarek
-    debtor_transfer_item = debtor_transfer.transfer_items.first
+    debtor_transfer_item = debtor_transfer.transfer_items.find(:first, :conditions => 'value > 0')
 
     #none
     save_simple_transfer(:outcome => rupert.asset, :income => rupert.loan.children.second) #move money to mateusz
     save_simple_transfer(:income => rupert.asset, :outcome => rupert.loan.children.second) #mateusz gives me money back
 
+    # creditor takes and gives money back
+    save_simple_transfer(:outcome => rupert.asset, :income => rupert.loan.children.third, :currency => @dolar, :day => 5.days.ago.to_date) #move money to marta
+    save_simple_transfer(:income => rupert.asset, :outcome => rupert.loan.children.third, :currency => @dolar, :day => 4.days.ago.to_date) #marta gives me money back
     #my creditor
-    creditor_transfer = save_simple_transfer(:income => rupert.asset, :outcome => rupert.loan.children.third)  #martga gives me some money
+    creditor_transfer = save_simple_transfer(:income => rupert.asset, :outcome => rupert.loan.children.third, :currency => @euro, :day => Date.today)  #martga gives me some euro money
+    creditor_transfer_item = creditor_transfer.transfer_items.find(:first, :conditions => 'value < 0')
+    
 
     #move money to bank that is not a loan category because rupert does not communicate with them via mails
     save_simple_transfer(:outcome => rupert.asset, :income => rupert.loan.children.last)
 
+    #wewnatrz transferow normalne saldo
+    # obiekt money trzyma saldo zawsze w wartosciach dodatnich.
     get :test
     assert_equal [{:loan => rupert.loan.children.first, :money => Money.new(debtor_transfer_item.currency => debtor_transfer_item.value), :transfers => [{:transfer => debtor_transfer, :saldo => Money.new(@zloty => debtor_transfer_item.value) }]}], assigns(:debtors)
-    assert_not_nil assigns(:creditors)
+    assert_equal [{:loan => rupert.loan.children.third, :money => Money.new(creditor_transfer_item.currency => -creditor_transfer_item.value), :transfers => [{:transfer => creditor_transfer, :saldo => Money.new(@euro => creditor_transfer_item.value) }]}], assigns(:creditors)
   end
 end
