55e94e0a4de17d49f5780ac04c6a8d9c69d3e202
Robert Pankowecki <robert.pankowecki@gmail.com>
2009-03-13 13:45:34 +0000

    Shadow: better
    git-svn-id: svn+ssh://s.rootnode.pl/home/rupert/svn/manage_my_money/code@439 e449e79d-dacd-4ad4-aaaf-698cbad89799

--- app/helpers/shadow_helper.rb
+++ app/helpers/shadow_helper.rb
@@ -3,24 +3,42 @@ module ShadowHelper
   # Creates a table that includes elements created in block and
   # surrounds them with shadow
   #
-  # ==== Signatures
-  #
-  # shadow(*exclude_array) do
-  #   # some text, div, span, table, whatever
-  # end
-  #
   # ==== Options
   # * top
   # * bottom
   # * left
   # * right
-  def shadow(*exclude_array, &block)
+  #
+  # ==== Examples
+  #
+  # shadow do
+  #   <div>sth</div>
+  # end
+  #
+  # shadow(:top, :bottom) do
+  #   <div>sth</div>
+  # end
+  #
+  # shadow(:left, :style => "background-color: white;") do
+  #   <div>This element will has white background</div>
+  # end
+  #
+  # shadow(:left, {:id => 'shadow-table-without-left-shadow'}, {:style => "background-color: white;", :id => 'inner-td-of-shadowed-table'}) do
+  #   <div>This element will has white background</div>
+  # end
+
+  def shadow(*args, &block)
+    content_options = args.extract_options!
+    table_options = args.extract_options!
+
+    table_options[:class] ||= 'shadow'
+    content_options[:class] ||= 'mm-shadow'
     raise 'No block given when required' unless block_given?
 
-    top = !exclude_array.include?(:top)
-    bottom = !exclude_array.include?(:bottom)
-    left = !exclude_array.include?(:left)
-    right = !exclude_array.include?(:right)
+    top = !args.include?(:top)
+    bottom = !args.include?(:bottom)
+    left = !args.include?(:left)
+    right = !args.include?(:right)
 
     text = ''
 
@@ -37,7 +55,7 @@ module ShadowHelper
     begin
       tr = ''
       tr << "<td class=\"ml-shadow shadow-y\"> </td>" if left
-      tr << "<td class=\"mm-shadow\">#{capture(&block)}</td>"
+      tr << content_tag(:td, capture(&block), content_options)
       tr << "<td class=\"mr-shadow shadow-y\"> </td>" if right
       text << "<tr>#{tr}</tr>"
     end
@@ -50,6 +68,7 @@ module ShadowHelper
       text << "<tr>#{tr}</tr>"
     end
 
-    concat "<table class=\"shadow\" > #{text} </table>"
+    #concat "<table class=\"shadow\" > #{text} </table>"
+    concat content_tag(:table, text, table_options)
   end
 end
