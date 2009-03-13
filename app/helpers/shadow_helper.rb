4b0068489bc40a022cc426650d2c261483958015
Robert Pankowecki <robert.pankowecki@gmail.com>
2009-03-13 15:22:51 +0000

    Shadow: uporządkowanie + lekki cień bez rogów
    git-svn-id: svn+ssh://s.rootnode.pl/home/rupert/svn/manage_my_money/code@441 e449e79d-dacd-4ad4-aaaf-698cbad89799

--- app/helpers/shadow_helper.rb
+++ app/helpers/shadow_helper.rb
@@ -1,4 +1,16 @@
 module ShadowHelper
+
+
+  def light_shadow(*args, &block)
+    shadow('60blue', *args, &block)
+  end
+
+
+  def strong_shadow(*args, &block)
+    shadow('80grey', *args, &block)
+  end
+
+  alias_method :dark_shadow, :strong_shadow
   
   # Creates a table that includes elements created in block and
   # surrounds them with shadow
@@ -11,23 +23,26 @@ module ShadowHelper
   #
   # ==== Examples
   #
-  # shadow do
+  # shadow('80grey' do
   #   <div>sth</div>
   # end
   #
-  # shadow(:top, :bottom) do
-  #   <div>sth</div>
+  # shadow('80grey', :top, :bottom) do
+  #   <div>Div without top and bottom shadow</div>
   # end
   #
-  # shadow(:left, :style => "background-color: white;") do
+  # shadow('60blue', :left, :style => "background-color: white;") do
   #   <div>This element will has white background</div>
   # end
   #
-  # shadow(:left, {:id => 'shadow-table-without-left-shadow'}, {:style => "background-color: white;", :id => 'inner-td-of-shadowed-table'}) do
+  # shadow('60blue', :left, {:id => 'shadow-table-without-left-shadow'}, {:style => "background-color: white;", :id => 'inner-td-of-shadowed-table'}) do
   #   <div>This element will has white background</div>
   # end
 
   def shadow(*args, &block)
+    style = args.shift
+    raise 'Wrong argument' unless style.is_a? String
+
     content_options = args.extract_options!
     table_options = args.extract_options!
 
@@ -45,26 +60,26 @@ module ShadowHelper
     #top
     if top
       tr = ''
-      tr << "<td class=\"tl-shadow shadow-x shadow-y\"> </td>" if left
-      tr << "<td class=\"tm-shadow shadow-x\"> </td>"
-      tr << "<td class=\"tr-shadow shadow-x shadow-y\"> </td>" if right
+      tr << "<td class=\"tl-shadow-#{style} shadow-x shadow-y\"> </td>" if left
+      tr << "<td class=\"tm-shadow-#{style} shadow-x\"> </td>"
+      tr << "<td class=\"tr-shadow-#{style} shadow-x shadow-y\"> </td>" if right
       text << "<tr>#{tr}</tr>"
     end
 
     #middle
     begin
       tr = ''
-      tr << "<td class=\"ml-shadow shadow-y\"> </td>" if left
+      tr << "<td class=\"ml-shadow-#{style} shadow-y\"> </td>" if left
       tr << content_tag(:td, capture(&block), content_options)
-      tr << "<td class=\"mr-shadow shadow-y\"> </td>" if right
+      tr << "<td class=\"mr-shadow-#{style} shadow-y\"> </td>" if right
       text << "<tr>#{tr}</tr>"
     end
 
     if bottom
       tr = ''
-      tr << "<td class=\"bl-shadow shadow-x shadow-y\"> </td>" if left
-      tr << "<td class=\"bm-shadow shadow-x\"> </td>"
-      tr << "<td class=\"br-shadow shadow-x shadow-y\"> </td>" if right
+      tr << "<td class=\"bl-shadow-#{style} shadow-x shadow-y\"> </td>" if left
+      tr << "<td class=\"bm-shadow-#{style} shadow-x\"> </td>"
+      tr << "<td class=\"br-shadow-#{style} shadow-x shadow-y\"> </td>" if right
       text << "<tr>#{tr}</tr>"
     end
 
