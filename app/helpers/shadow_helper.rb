96f8c127e9db10b34d0d87b9b377702c7a518943
Robert Pankowecki <robert.pankowecki@gmail.com>
2009-03-14 17:17:12 +0000

    Inner shadow.
    git-svn-id: svn+ssh://s.rootnode.pl/home/rupert/svn/manage_my_money/code@453 e449e79d-dacd-4ad4-aaaf-698cbad89799

--- app/helpers/shadow_helper.rb
+++ app/helpers/shadow_helper.rb
@@ -1,6 +1,37 @@
 module ShadowHelper
 
 
+  # Creates a table that includes elements created in block and
+  # surrounds them with inner shadow
+  #
+  # ==== Options
+  # * top
+  # * bottom
+  # * left
+  # * right
+  #
+  # ==== Examples
+  #
+  # inner_shadow do
+  #   <div>sth</div>
+  # end
+  #
+  # inner_shadow :top, :bottom) do
+  #   <div>Div without top and bottom inner shadow</div>
+  # end
+  #
+  # inner_shadow :left, :style => "background-color: white;") do
+  #   <div>This element will has white background</div>
+  # end
+  #
+  # inner_shadow :left, {:id => 'shadow-table-without-left-shadow'}, {:style => "background-color: white;", :id => 'inner-td-of-shadowed-table'}) do
+  #   <div>This element will has white background</div>
+  # end
+  def inner_shadow(*args, &block)
+    inner_or_outer_shadow(:inner, '80grey', *args, &block)
+  end
+
+
   def light_shadow(*args, &block)
     shadow('60blue', *args, &block)
   end
@@ -13,7 +44,7 @@ module ShadowHelper
   alias_method :dark_shadow, :strong_shadow
   
   # Creates a table that includes elements created in block and
-  # surrounds them with shadow
+  # surrounds them with outer shadow
   #
   # ==== Options
   # * top
@@ -38,8 +69,24 @@ module ShadowHelper
   # shadow('60blue', :left, {:id => 'shadow-table-without-left-shadow'}, {:style => "background-color: white;", :id => 'inner-td-of-shadowed-table'}) do
   #   <div>This element will has white background</div>
   # end
-
   def shadow(*args, &block)
+    inner_or_outer_shadow(:outer, *args, &block)
+  end
+
+
+  private
+
+  # inner_or_outer_shadow(:inner, '80grey', :left, :right ... table-options, element-options
+  # inner_or_outer_shadow(:outer, '60blue' ... element-options
+  def inner_or_outer_shadow(*args, &block)
+    type = args.shift
+
+    type = case type
+    when :inner then 'inner-shadow'
+    when :outer then 'shadow'
+    else raise 'Unknown shadow type'
+    end
+
     style = args.shift
     raise 'Wrong argument' unless style.is_a? String
 
@@ -60,26 +107,26 @@ module ShadowHelper
     #top
     if top
       tr = ''
-      tr << "<td class=\"tl-shadow-#{style} shadow-x shadow-y\"> </td>" if left
-      tr << "<td class=\"tm-shadow-#{style} shadow-x\"> </td>"
-      tr << "<td class=\"tr-shadow-#{style} shadow-x shadow-y\"> </td>" if right
+      tr << "<td class=\"tl-#{type}-#{style} shadow-x shadow-y\"> </td>" if left
+      tr << "<td class=\"tm-#{type}-#{style} shadow-x\"> </td>"
+      tr << "<td class=\"tr-#{type}-#{style} shadow-x shadow-y\"> </td>" if right
       text << "<tr>#{tr}</tr>"
     end
 
     #middle
     begin
       tr = ''
-      tr << "<td class=\"ml-shadow-#{style} shadow-y\"> </td>" if left
+      tr << "<td class=\"ml-#{type}-#{style} shadow-y\"> </td>" if left
       tr << content_tag(:td, capture(&block), content_options)
-      tr << "<td class=\"mr-shadow-#{style} shadow-y\"> </td>" if right
+      tr << "<td class=\"mr-#{type}-#{style} shadow-y\"> </td>" if right
       text << "<tr>#{tr}</tr>"
     end
 
     if bottom
       tr = ''
-      tr << "<td class=\"bl-shadow-#{style} shadow-x shadow-y\"> </td>" if left
-      tr << "<td class=\"bm-shadow-#{style} shadow-x\"> </td>"
-      tr << "<td class=\"br-shadow-#{style} shadow-x shadow-y\"> </td>" if right
+      tr << "<td class=\"bl-#{type}-#{style} shadow-x shadow-y\"> </td>" if left
+      tr << "<td class=\"bm-#{type}-#{style} shadow-x\"> </td>"
+      tr << "<td class=\"br-#{type}-#{style} shadow-x shadow-y\"> </td>" if right
       text << "<tr>#{tr}</tr>"
     end
 
