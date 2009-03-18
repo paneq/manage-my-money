e850ddecfa32eb8dc8a3b08d2fc21edb1c490124
Robert Pankowecki <robert.pankowecki@gmail.com>
2009-03-18 17:19:11 +0000

    Odkomentowanie testow przechodzacych po menu, ktore w poprzednim commicie zostały przypadkiem zakomentowane.
    git-svn-id: svn+ssh://s.rootnode.pl/home/rupert/svn/manage_my_money/code@471 e449e79d-dacd-4ad4-aaaf-698cbad89799

--- test/selenium/menu_test.rb
+++ test/selenium/menu_test.rb
@@ -34,70 +34,70 @@ begin
     end
 
 
-    #Test user can click on each link on main page including top categories from quick acces
-    #    def test_main_page_links
-    #
-    #      go_home
-    #      @selenium.click "kategorie-menu-link"
-    #      @selenium.wait_for_page_to_load "30000"
-    #      selenium_assert {
-    #        assert @selenium.is_text_present("Twoje kategorie")
-    #      }
-    #
-    #      go_home
-    #      @selenium.click "transfery-menu-link"
-    #      @selenium.wait_for_page_to_load "30000"
-    #      selenium_assert {
-    #        assert @selenium.is_text_present("Lista transferów")
-    #      }
-    #
-    #      go_home
-    #      @selenium.click "waluty-menu-link"
-    #      @selenium.wait_for_page_to_load "30000"
-    #      selenium_assert {
-    #        assert @selenium.is_text_present("Waluty")
-    #      }
-    #
-    #      go_home
-    #      @selenium.click "raporty-menu-link"
-    #      @selenium.wait_for_page_to_load "30000"
-    #      selenium_assert {
-    #        assert @selenium.is_text_present("Raporty")
-    #      }
-    #
-    #      go_home
-    #      @selenium.click "edycja-ustawien-menu-link"
-    #      @selenium.wait_for_page_to_load "30000"
-    #      selenium_assert {
-    #        assert @selenium.is_text_present("Edycja ustawień")
-    #      }
-    #
-    #      go_home
-    #      @selenium.click "//img[@alt='Manage My Money Logo']"
-    #      @selenium.wait_for_page_to_load "30000"
-    #      selenium_assert {
-    #        assert @selenium.is_text_present("Twoje kategorie")
-    #        assert @selenium.is_text_present("Zalogowany: #{@rupert.login}")
-    #      }
-    #
-    #
-    #      @rupert.categories.top.each do |cat|
-    #        go_home
-    #        @selenium.click "category-in-menu-#{cat.id}"
-    #        @selenium.wait_for_page_to_load "30000"
-    #        selenium_assert {
-    #          assert @selenium.is_text_present("#{cat.name} [Edycja]")
-    #        }
-    #      end
-    #
-    #      go_home
-    #      @selenium.click "wyloguj-menu-link"
-    #      @selenium.wait_for_page_to_load "30000"
-    #      selenium_assert {
-    #        assert @selenium.is_text_present("Logowanie")
-    #        assert @selenium.is_text_present("Zaloguj się!")
-    #      }
-    #    end
+    # Test user can click on each link on main page including top categories from quick acces
+    def test_main_page_links
+    
+      go_home
+      @selenium.click "kategorie-menu-link"
+      @selenium.wait_for_page_to_load "30000"
+      selenium_assert {
+        assert @selenium.is_text_present("Twoje kategorie")
+      }
+    
+      go_home
+      @selenium.click "transfery-menu-link"
+      @selenium.wait_for_page_to_load "30000"
+      selenium_assert {
+        assert @selenium.is_text_present("Lista transferów")
+      }
+    
+      go_home
+      @selenium.click "waluty-menu-link"
+      @selenium.wait_for_page_to_load "30000"
+      selenium_assert {
+        assert @selenium.is_text_present("Waluty")
+      }
+    
+      go_home
+      @selenium.click "raporty-menu-link"
+      @selenium.wait_for_page_to_load "30000"
+      selenium_assert {
+        assert @selenium.is_text_present("Raporty")
+      }
+    
+      go_home
+      @selenium.click "edycja-ustawien-menu-link"
+      @selenium.wait_for_page_to_load "30000"
+      selenium_assert {
+        assert @selenium.is_text_present("Edycja ustawień")
+      }
+    
+      go_home
+      @selenium.click "//img[@alt='Manage My Money Logo']"
+      @selenium.wait_for_page_to_load "30000"
+      selenium_assert {
+        assert @selenium.is_text_present("Twoje kategorie")
+        assert @selenium.is_text_present("Zalogowany: #{@rupert.login}")
+      }
+    
+    
+      @rupert.categories.top.each do |cat|
+        go_home
+        @selenium.click "category-in-menu-#{cat.id}"
+        @selenium.wait_for_page_to_load "30000"
+        selenium_assert {
+          assert @selenium.is_text_present("#{cat.name} [Edycja]")
+        }
+      end
+    
+      go_home
+      @selenium.click "wyloguj-menu-link"
+      @selenium.wait_for_page_to_load "30000"
+      selenium_assert {
+        assert @selenium.is_text_present("Logowanie")
+        assert @selenium.is_text_present("Zaloguj się!")
+      }
+    end
 
 
     def test_uberfast_category_search_in_menu
