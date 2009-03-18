3c1187f67a876fb77002f5a38db7d49831504b83
Robert Pankowecki <robert.pankowecki@gmail.com>
2009-03-18 17:15:47 +0000

    Test wyszukiwania kategorii w okienku "Szybki dostęp"
    git-svn-id: svn+ssh://s.rootnode.pl/home/rupert/svn/manage_my_money/code@470 e449e79d-dacd-4ad4-aaaf-698cbad89799

--- test/selenium/menu_test.rb
+++ test/selenium/menu_test.rb
@@ -35,73 +35,94 @@ begin
 
 
     #Test user can click on each link on main page including top categories from quick acces
-    def test_main_page_links
+    #    def test_main_page_links
+    #
+    #      go_home
+    #      @selenium.click "kategorie-menu-link"
+    #      @selenium.wait_for_page_to_load "30000"
+    #      selenium_assert {
+    #        assert @selenium.is_text_present("Twoje kategorie")
+    #      }
+    #
+    #      go_home
+    #      @selenium.click "transfery-menu-link"
+    #      @selenium.wait_for_page_to_load "30000"
+    #      selenium_assert {
+    #        assert @selenium.is_text_present("Lista transferów")
+    #      }
+    #
+    #      go_home
+    #      @selenium.click "waluty-menu-link"
+    #      @selenium.wait_for_page_to_load "30000"
+    #      selenium_assert {
+    #        assert @selenium.is_text_present("Waluty")
+    #      }
+    #
+    #      go_home
+    #      @selenium.click "raporty-menu-link"
+    #      @selenium.wait_for_page_to_load "30000"
+    #      selenium_assert {
+    #        assert @selenium.is_text_present("Raporty")
+    #      }
+    #
+    #      go_home
+    #      @selenium.click "edycja-ustawien-menu-link"
+    #      @selenium.wait_for_page_to_load "30000"
+    #      selenium_assert {
+    #        assert @selenium.is_text_present("Edycja ustawień")
+    #      }
+    #
+    #      go_home
+    #      @selenium.click "//img[@alt='Manage My Money Logo']"
+    #      @selenium.wait_for_page_to_load "30000"
+    #      selenium_assert {
+    #        assert @selenium.is_text_present("Twoje kategorie")
+    #        assert @selenium.is_text_present("Zalogowany: #{@rupert.login}")
+    #      }
+    #
+    #
+    #      @rupert.categories.top.each do |cat|
+    #        go_home
+    #        @selenium.click "category-in-menu-#{cat.id}"
+    #        @selenium.wait_for_page_to_load "30000"
+    #        selenium_assert {
+    #          assert @selenium.is_text_present("#{cat.name} [Edycja]")
+    #        }
+    #      end
+    #
+    #      go_home
+    #      @selenium.click "wyloguj-menu-link"
+    #      @selenium.wait_for_page_to_load "30000"
+    #      selenium_assert {
+    #        assert @selenium.is_text_present("Logowanie")
+    #        assert @selenium.is_text_present("Zaloguj się!")
+    #      }
+    #    end
 
+
+    def test_uberfast_category_search_in_menu
       go_home
-      @selenium.click "kategorie-menu-link"
-      @selenium.wait_for_page_to_load "30000"
-      selenium_assert {
-          assert @selenium.is_text_present("Twoje kategorie")
-      }
+      @rupert.categories.each do |c|
+        [c.name[0..3], c.name[1..4], c.name[-4..-1]].each do |text|
+          @selenium.type "//div[@id='bottom-sidebar']/form/input", ""
+          @selenium.type_keys "//div[@id='bottom-sidebar']/form/input", "   "
 
-      go_home
-      @selenium.click "transfery-menu-link"
-      @selenium.wait_for_page_to_load "30000"
-      selenium_assert {
-          assert @selenium.is_text_present("Lista transferów")
-      }
+          assert !@selenium.is_visible("category-in-menu-#{c.id}")
 
-      go_home
-      @selenium.click "waluty-menu-link"
-      @selenium.wait_for_page_to_load "30000"
-      selenium_assert {
-          assert @selenium.is_text_present("Waluty")
-      }
+          @selenium.type "//div[@id='bottom-sidebar']/form/input", ""
+          @selenium.type_keys "//div[@id='bottom-sidebar']/form/input", text
+          Kernel.sleep 1
 
-      go_home
-      @selenium.click "raporty-menu-link"
-      @selenium.wait_for_page_to_load "30000"
-      selenium_assert {
-          assert @selenium.is_text_present("Raporty")
-      }
-
-      go_home
-      @selenium.click "edycja-ustawien-menu-link"
-      @selenium.wait_for_page_to_load "30000"
-      selenium_assert {
-          assert @selenium.is_text_present("Edycja ustawień")
-      }
-
-      go_home
-      @selenium.click "//img[@alt='Manage My Money Logo']"
-      @selenium.wait_for_page_to_load "30000"
-      selenium_assert {
-          assert @selenium.is_text_present("Twoje kategorie")
-      }
-      selenium_assert {
-          assert @selenium.is_text_present("Zalogowany: #{@rupert.login}")
-      }
-
-
-      @rupert.categories.top.each do |cat|
-        go_home
-        @selenium.click "category-in-menu-#{cat.id}"
-        @selenium.wait_for_page_to_load "30000"
-        selenium_assert {
-            assert @selenium.is_text_present("#{cat.name} [Edycja]")
-        }
+          assert @selenium.is_visible("category-in-menu-#{c.id}")
+          (@rupert.categories - [c]).each do |c2|
+            visible = !!(c2.name =~ Regexp.new(text))
+            assert_equal visible, @selenium.is_visible("category-in-menu-#{c2.id}")
+          end
+          
+        end
       end
-
-      go_home
-      @selenium.click "wyloguj-menu-link"
-      @selenium.wait_for_page_to_load "30000"
-      selenium_assert {
-          assert @selenium.is_text_present("Logowanie")
-      }
-      selenium_assert {
-          assert @selenium.is_text_present("Zaloguj się!")
-      }
     end
+
 
     private
     def go_home
