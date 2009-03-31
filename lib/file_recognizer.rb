cc76b80de53b26062bfeb5a387dc14811884b357
Robert Pankowecki <robert.pankowecki@gmail.com>
2009-04-01 00:42:02 +0200

    Comparing with downcased version of string becuase files can have uppercased and downcased extensions like XML and xml

--- lib/file_recognizer.rb
+++ lib/file_recognizer.rb
@@ -1,7 +1,7 @@
 module FileRecognizer
   def recognize_file(content, file_name)
-    return :inteligo if file_name.ends_with? 'xml'
-    return :mbank if file_name.ends_with? 'csv'
+    return :inteligo if file_name.downcase.ends_with? 'xml'
+    return :mbank if file_name.downcase.ends_with? 'csv'
     return nil
   end
 end
