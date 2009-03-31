7218faae43754e73f4b7c162e37e4c6703cfaa8e
Jarosław Plebański <sejtenik@gmail.com>
2009-04-01 01:16:07 +0200

    Testing testing testing

--- lib/gnucash_parser.rb
+++ lib/gnucash_parser.rb
@@ -38,10 +38,21 @@ class GnucashParser
   }
 
   class << self
+
+    # result format:
+    # { :categories => {:in_file => X, :added => X, :merged => X, :errors => ['Asd', 'dfg']}
+    #   :transfers => {:in_file => X, :added => X, :errors => ['Asd', 'dfg']}
+    # }
+    #
+    #
     def parse(content, user)
       doc = Nokogiri::XML(content)
-      import_categories(user, doc)
-      import_transfers(user, doc)
+      result = {}
+      result[:categories] = import_categories(user, doc)
+      result[:transfers] = import_transfers(user, doc)
+      result
+    rescue Exception => e
+      raise GnuCashParseError.new(e)
     end
 
     def logger
@@ -51,21 +62,21 @@ class GnucashParser
 
     def import_categories(user, doc)
       
-
-      accounts_count = doc.find('//gnc:count-data[@cd:type="account"]').inner_html.to_i
+      result = {}
+      accounts_count = doc.find('//gnc:count-data[@cd:type="account"]').inner_text.to_i
 
       root_category = nil
       categories = SequencedHash.new
       top_categories = {}
       logger.debug "\n==Parsing categories"
       doc.find('//gnc:account').each do |node|
-        #       logger.debug "Commodity: " + node.find('act:commodity/cmdty:id').inner_html
+        #       logger.debug "Commodity: " + node.find('act:commodity/cmdty:id').inner_text
         c = Category.new
-        c.import_guid = node.find('act:id').inner_html
-        c.name = node.find('act:name').inner_html
-        c.description = node.find('act:description').inner_html
-        c.parent_guid = node.find('act:parent').inner_html
-        type = node.find('act:type').inner_html
+        c.import_guid = node.find('act:id').inner_text
+        c.name = node.find('act:name').inner_text
+        c.description = node.find('act:description').inner_text
+        c.parent_guid = node.find('act:parent').inner_text
+        type = node.find('act:type').inner_text
         c.user = user
         c.imported = true
 
@@ -86,6 +97,7 @@ class GnucashParser
 
       #kategorie poziomu głownego (parent_id == root) przeniesc do naszych odpowiednich kategorii
       #chyba ze sa tylko po jednej - wtedy je utozsamiamy bez zmiany nazwy?
+
       logger.debug "\n==Merging top categories"
       [:ASSET, :INCOME, :EXPENSE, :LOAN, :BALANCE ].each do |category_type|
         top = user.categories.top.of_type(category_type).find(:first)
@@ -113,45 +125,61 @@ class GnucashParser
       end
 
       saved = 0
+      merged = 0
+      result[:errors] = []
       logger.debug "\n==Saving categories"
       categories.each_value do |cat|
         existing_cat = user.categories.find_by_import_guid(cat.import_guid)
         unless existing_cat
           print '.'; STDOUT.flush
+          new_record = cat.new_record?
           if cat.save
             print '.'; STDOUT.flush
-            saved += 1
+            if new_record
+              saved += 1
+            else
+              merged += 1
+            end
+
           else
             print 'x'; STDOUT.flush
           end
         end
       end
       logger.debug "\nSaved #{saved} categories from #{accounts_count}"
+
+      result[:in_file] = accounts_count - 1 #because of ROOT category
+      result[:added] = saved
+      result[:merged] = merged
+
+      return result
     end
 
 
     def import_transfers(user, doc)
-      transaction_count = doc.find('//gnc:count-data[@cd:type="transaction"]').inner_html.to_i
+      result = {}
+      transaction_count = doc.find('//gnc:count-data[@cd:type="transaction"]').inner_text.to_i
       saved = 0
       logger.debug "\n==Parsing and saving transfers"
+      result[:errors] = []
       doc.find('//gnc:transaction').each do |node|
         t = Transfer.new
         t.user = user
-        t.import_guid = node.find('trn:id').inner_html
-        date = node.find('trn:date-posted/ts:date').inner_html
+        t.import_guid = node.find('trn:id').inner_text
+        date = node.find('trn:date-posted/ts:date').inner_text
         t.day = Date.parse date
-        t.description = node.find('trn:description').inner_html
+        t.description = node.find('trn:description').inner_text
 
-        currency_name = node.find('trn:currency/cmdty:id').inner_html
+        currency_name = node.find('trn:currency/cmdty:id').inner_text
         currency = Currency.find(:first, :conditions => ['long_symbol = ? AND (user_id IS NULL OR user_id = ?)', currency_name, user.id])
 
         node.find('trn:splits/trn:split').each do |split|
           ti = TransferItem.new
-          ti.import_guid = split.find('split:id').inner_html
-          category_guid = split.find('split:account').inner_html
+          ti.import_guid = split.find('split:id').inner_text
+          category_guid = split.find('split:account').inner_text
           ti.category = user.categories.find_by_import_guid(category_guid)
-          ti.description = split.find('split:memo').inner_html
-          value_str = split.find('split:value').inner_html
+          ti.description = split.find('split:memo').inner_text
+          value_str = split.find('split:value').inner_text
           if value_str =~ /(-?\d*)\/(\d*)/
             ti.value = $1.to_f / $2.to_f
           else
@@ -176,6 +204,10 @@ class GnucashParser
 
       end
       logger.debug "\nSaved #{saved} transfers from #{transaction_count}"
+      result[:in_file] = transaction_count
+      result[:added] = saved
+
+      return result
     end
 
 
