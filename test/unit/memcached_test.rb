e742b87017ce926a373550341dfb1bdc38bbbed4
Jarosław Plebański <sejtenik@gmail.com>
2009-04-07 10:38:30 +0200

    Escaping user input in goals ang gnucash import

--- test/unit/memcached_test.rb
+++ test/unit/memcached_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class MemcachedTest < ActiveSupport::TestCase
   def test_memcached_write_and_read
     require_memcached
+    sleep 1
     assert_equal true, Rails.cache.write('key', '123456')
     assert_equal '123456', Rails.cache.read('key')
   end
