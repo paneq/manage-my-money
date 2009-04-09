ca832fb07bc22b7429afd55980f51288be5250cc
Robert Pankowecki <robert.pankowecki@gmail.com>
2009-04-09 19:44:38 +0200

    Improved documentation of validates_equality with examples.

--- vendor/plugins/validates_equality/lib/validates_equality.rb
+++ vendor/plugins/validates_equality/lib/validates_equality.rb
@@ -7,11 +7,46 @@ module ValidatesEquality
 
   module ClassMethods
 
+    # Validates that the specified attribute(first paramter) is the same in all given assosciation
+    #
+    # class Transfer < ActiveRecord::Base
+    #   validates_equality :user_id, :category
+    #   validates_equality :user_id, :transfer_items
+    #   validates_equality :user_id, [:transfer_items, :category]
+    #   validates_equality :user_id, [:transfer_items, :tags]
+    # end
+    #
+    # or
+    #
+    # class Transfer < ActiveRecord::Base
+    #   validates_equality :user_id, :category, :transfer_items, [:transfer_items, :category], [:transfer_items, :tags]
+    # end
+    #
+    # is the same as checking if
+    #
+    # transfer.user_id == transfer.category.user_id
+    #
+    # transfer.transfer_items.each do |ti|
+    #   transfer.user_id == ti.user_id
+    # end
+    # 
+    # transfer.transfer_items.each do |ti|
+    #   transfer.user_id == ti.category.user_id
+    # end
+    #
+    # transfer.transfer_items.each do |ti|
+    #   ti.tags.each do |tag|
+    #     transfer.user_id == tag.user_id
+    #   end
+    # end
+    #
+    # If any of this condition is evaluated to false error on transfer.user_id attribute is added.
+    #
     # Options:
-    # * <tt>:raise_nil_in_chain</tt> - If set to true exception is raise when any association is nil
+    # * <tt>:raise_nil_in_chain</tt> - If set to true exception is raise when any association in chain is nil
     # * <tt>:on</tt> - Specifies when this validation is active (default is <tt>:save</tt>, other options <tt>:create</tt>, <tt>:update</tt>).
-    # * <tt>:allow_nil</tt> - Skip validation if attribute is +nil+.
-    # * <tt>:allow_blank</tt> - Skip validation if attribute is blank.
+    # * <tt>:allow_nil</tt> - Skip validation if attribute value of last association is +nil+.
+    # * <tt>:allow_blank</tt> - Skip validation if attribute value of last association is blank.
     # * <tt>:if</tt> - Specifies a method, proc or string to call to determine if the validation should
     #   occur (e.g. <tt>:if => :allow_validation</tt>, or <tt>:if => Proc.new { |user| user.signup_step > 2 }</tt>).  The
     #   method, proc or string should return or evaluate to a true or false value.
@@ -72,17 +107,7 @@ module ValidatesEquality
     end
 
 
-    # Options:
-    # * <tt>:raise_nil_in_chain</tt> - If set to true exception is raise when any association is nil
-    # * <tt>:on</tt> - Specifies when this validation is active (default is <tt>:save</tt>, other options <tt>:create</tt>, <tt>:update</tt>).
-    # * <tt>:allow_nil</tt> - Skip validation if attribute is +nil+.
-    # * <tt>:allow_blank</tt> - Skip validation if attribute is blank.
-    # * <tt>:if</tt> - Specifies a method, proc or string to call to determine if the validation should
-    #   occur (e.g. <tt>:if => :allow_validation</tt>, or <tt>:if => Proc.new { |user| user.signup_step > 2 }</tt>).  The
-    #   method, proc or string should return or evaluate to a true or false value.
-    # * <tt>:unless</tt> - Specifies a method, proc or string to call to determine if the validation should
-    #   not occur (e.g. <tt>:unless => :skip_validation</tt>, or <tt>:unless => Proc.new { |user| user.signup_step <= 2 }</tt>).  The
-    #   method, proc or string should return or evaluate to a true or false value.
+    # Same as: validates_equality :user_id, attr_paths
     def validates_user_id(*attr_paths)
       validates_equality(:user_id, *attr_paths)
     end
